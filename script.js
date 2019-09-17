b8f07b48f8eb19b0f7083f8c6f35d06008252eb6
LukasFrm <lukas.freimonas@gmail.com>
2019-09-17 23:59:43 +0300

    456

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-// MAIN FUNCTION
-var arrayWinnableText = "";
+// MAIN FUNCTIONS
+function clearData() {
+  document.getElementById("optimalRouteDiv").innerHTML = "";
+  document.getElementById("arrayEntered").innerHTML = "";
+  document.getElementById("searchTxt").value = "";
+  document.getElementById("arrayWinnable").innerHTML = "";
+}
 
+var arrayWinnableText = "";
 function arrayUnwinnable() {
-  alert("Array is unwinnable");
-  clearData();
+  var arrayWinnableText = "NO";
+  document.getElementById(
+    "arrayWinnable"
+  ).innerHTML = `<b>${arrayWinnableText}</b>`;
+  document.getElementById("arrayWinnable").style.color = "red";
+
+  // clearData();
 }
 
 function arrayWinnable() {
-  alert("Array is winnable!");
+  var arrayWinnableText = "YES";
+  document.getElementById("arrayWinnable").innerHTML = `${arrayWinnableText}`;
 }
 
 function arraySubmitted() {
@@ -17,7 +29,7 @@ function arraySubmitted() {
   }
 
   //Extract the input value
-  else var value = document.getElementById("searchTxt").value;
+  var value = document.getElementById("searchTxt").value;
 
   //Convert it to an array
   var array = new Array();
@@ -26,28 +38,25 @@ function arraySubmitted() {
     array.push(parseInt(item));
   });
 
+  // Check if array doesn't start with 0
   if (array[0] === 0) {
     alert(
       "Array unwinnable. Please check, if the first value in your array should truly be 0"
     );
     clearData();
-
-    return (arrayWinnable = false);
   }
 
   //  Check if all indexes are numbers
   for (i = 0; i < array.length; i++) {
     if (isNaN(array[i])) {
-      alert("All values must be numbers");
+      alert("All values must be integer numbers");
       clearData();
     }
   }
 
   var newPossibleSteps = new Array();
-  var optimalArray = new Array();
   var trueValues = new Array();
   var trueArrayValues = new Array();
-  var optimalRouteDisplay;
 
   //Create an array representation with each array number in a span element
   // most optimal numbers will be marked red
@@ -65,6 +74,8 @@ function arraySubmitted() {
 
   // Check for the most optimal route
   for (i = 0; i < array.length; i++) {
+    console.log(`i is ${i}`);
+
     newPossibleSteps[i] = [];
     trueValues[i] = [];
     trueArrayValues[i] = [];
@@ -76,7 +87,15 @@ function arraySubmitted() {
     for (var j = 1; j < endOfSteps; j++) {
       var step = array[i + j];
       if (!step) {
-        console.log("step is undefined");
+        console.log(`step @ index ${i} and j ${j} is undefined`);
+        if (newPossibleSteps[i].length === 1) {
+          console.log(
+            `It has found the one! newPossibleSteps[i][0] is ${
+              newPossibleSteps[i][0]
+            } @ i ${i}`
+          );
+          trueValues[i].push(newPossibleSteps[i][0]);
+        }
       } else if (!newPossibleSteps[i]) {
         newPossibleSteps[i].push(step);
       } else if (newPossibleSteps[i]) {
@@ -98,66 +117,56 @@ function arraySubmitted() {
             var max = Math.max(...trueValues[i]);
             var biggestIndex = trueValues[i].indexOf(max);
             var calculatedOptimalStep = newPossibleSteps[i][biggestIndex];
-            optimalArray.push(calculatedOptimalStep);
             var highlightValue = trueArrayValues[i][biggestIndex];
 
             document
               .getElementsByTagName("span")
               .item(highlightValue).style.backgroundColor = "#8cff66";
-
           }
         }
       }
-
-    } 
-    if (parseInt(endOfSteps) === 1 ) {
-      trueValues[i].push(newPossibleSteps[i][0]);
     }
-     i += biggestIndex;
+    if (trueArrayValues[i] == "undefined" || trueArrayValues[i].length == 0) {
+      document.getElementsByTagName("span").item(i + 1).style.backgroundColor =
+        "red";
+    }
+
+    i += biggestIndex;
   }
 
-  // if (i === array.length - 1) {
-  // //When the loop is finished add first and last
-  // // index values of the array to the optimal array (only if array is winnable)
-  //   // optimalArray = [array[0], ...optimalArray, array[[array.length - 1]]];
-  // }
+  // NEGATIVE ARRAY FUNCTIONALITY
+
+  // Get array
+  var a = [-2, -5, -3, -1, -1, -1, -2, -1];
+  // Convert array into positive
+  var b = a.map(item => {
+    return (item *= -1);
+  });
+
+  // Reverse the array
+  var c = b.reverse();
+  
+  console.log(`c is ${c}`);
+  console.log(d);
 
   console.warn(trueValues);
   console.warn(newPossibleSteps);
   console.warn(trueArrayValues);
 
   document.getElementById("arrayEntered").innerHTML = array.toString();
-  document.getElementById("optimalRoute").innerHTML = optimalArray.toString();
 
   if (
     parseInt(trueArrayValues[trueArrayValues.length - 1]) ===
     array.length - 1
   ) {
     console.log("Array is winnable!");
-    arrayWinnable = true;
+    arrayWinnable();
 
     document.getElementsByTagName("span").item(0).style.backgroundColor =
       "#8cff66";
     document
       .getElementsByTagName("span")
       .item(array.length - 1).style.backgroundColor = "#8cff66";
-      return;
-  }
-
-  else arrayUnwinnable(); return
-
-  // if (arrayWinnable) {
-  //   document.getElementById("arrayWinnable").innerHTML =
-  //     '<span style="color:green"><b>YES</b></span>';
-  // } else
-  //   document.getElementById("arrayWinnable").innerHTML =
-  //     '<span style="color:red"><b>NO</b></span>';
-}
-
-function clearData() {
-  document.getElementById("optimalRoute").innerHTML = "";
-  document.getElementById("optimalRouteDiv").innerHTML = "";
-  document.getElementById("arrayEntered").innerHTML = "";
-  document.getElementById("searchTxt").value = "";
-  document.getElementById("arrayWinnable").innerHTML = "";
+    return;
+  } else arrayUnwinnable();
 }
